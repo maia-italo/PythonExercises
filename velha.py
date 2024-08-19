8184d220848ff23331ab23497084e3d2f2797fbf
Maia <155457715+maia-italo@users.noreply.github.com>
2024-08-19 08:15:59 -0300

    Update velha

--- velha.py
+++ velha.py
@@ -19,55 +19,55 @@ def jogar(troca):
 	troca = False
 	po = input(f'Qual posição deseja jogar [ {simb} ]? ')
 	if po == '1':
-		if t[0][0] == 'X' or t[0][0] == 'O':
+		if t[0][0] == '\033[1;31mX\033[m' or t[0][0] == '\033[1;36mO\033[m':
 			print('Jogada inválida ')
 		else:
 			t[0][0] = simb
 			troca = True
 	elif po == '2':
-		if t[0][1] == 'X' or t[0][1] == 'O':
+		if t[0][1] == '\033[1;31mX\033[m' or t[0][1] == '\033[1;36mO\033[m':
 			print('Jogada inválida ')
 		else:
 			t[0][1] = simb
 			troca = True
 	elif po == '3':
-		if t[0][2] == 'X' or t[0][2] == 'O':
+		if t[0][2] == '\033[1;31mX\033[m' or t[0][2] == '\033[1;36mO\033[m':
 			print('Jogada inválida ')
 		else:
 			t[0][2] = simb
 			troca = True
 	elif po == '4':
-		if t[1][0] == 'X' or t[1][0] == 'O':
+		if t[1][0] == '\033[1;31mX\033[m' or t[1][0] == '\033[1;36mO\033[m':
 			print('Jogada inválida ')
 		else:
 			t[1][0] = simb
 			troca = True
 	elif po == '5':
-		if t[1][1] == 'X' or t[1][1] == 'O':
+		if t[1][1] == '\033[1;31mX\033[m' or t[1][1] == '\033[1;36mO\033[m':
 			print('Jogada inválida ')
 		else:
 			t[1][1] = simb
 			troca = True
 	elif po == '6':
-		if t[1][2] == 'X' or t[1][2] == 'O':
+		if t[1][2] == '\033[1;31mX\033[m' or t[1][2] == '\033[1;36mO\033[m':
 			print('Jogada inválida ')
 		else:
 			t[1][2] = simb
 			troca = True
 	elif po == '7':
-		if t[2][0] == 'X' or t[2][0] == 'O':
+		if t[2][0] == '\033[1;31mX\033[m' or t[2][0] == '\033[1;36mO\033[m':
 			print('Jogada inválida ')
 		else:
 			t[2][0] = simb
 			troca = True
 	elif po == '8':
-		if t[2][1] == 'X' or t[2][1] == 'O':
+		if t[2][1] == '\033[1;31mX\033[m' or t[2][1] == '\033[1;36mO\033[m':
 			print('Jogada inválida ')
 		else:
 			t[2][1] = simb
 			troca = True
 	elif po == '9':
-		if t[2][2] == 'X' or t[2][2] == 'O':
+		if t[2][2] == '\033[1;31mX\033[m' or t[2][2] == '\033[1;36mO\033[m':
 			print('Jogada inválida ')
 		else:
 			t[2][2] = simb
@@ -78,10 +78,10 @@ def jogar(troca):
 
 
 def muda_j(s):
-	if s == 'X':
-		s = 'O'
+	if s == '\033[1;31mX\033[m':
+		s = '\033[1;36mO\033[m'
 	else:
-		s = 'X'
+		s = '\033[1;31mX\033[m'
 	return s
 
 
@@ -100,7 +100,7 @@ def fim_jogo():
 	ocorr = 0
 	for l in range(3):
 		for c in range(3):
-			if t[l][c] != 'X' and t[l][c] != 'O':
+			if t[l][c] != '\033[1;31mX\033[m' and t[l][c] != '\033[1;36mO\033[m':
 				ocorr += 1
 	if ocorr == 0:
 		val = 2
@@ -108,7 +108,7 @@ def fim_jogo():
 
 
 t = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
-simb = 'X'
+simb = '\033[1;31mX\033[m'
 
 while fim_jogo() == 0:
 	tabuleiro()
